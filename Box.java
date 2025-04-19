330396215ab01b28ca1fc481750c19bfdfcf2068
Nifla <fathimanifla1118@gmail.com>
2025-04-19 21:01:28 +0530

    Create Multiple object

--- Box.java
+++ Box.java
@@ -10,14 +10,25 @@ class Boxnew{
 
     public static void main(String[] args){
         Box newBox = new Box();
+        Box newBox2 = new Box();
+
         double volume;
+        
 
         
         newBox.width=10;
         newBox.height=12;
         newBox.depth=15;
 
+        newBox2.width=5;
+        newBox2.height=5;
+        newBox2.depth=5;
+
+
         volume=newBox.width*newBox.depth*newBox.height;
+        System.out.println("Volume is " + volume);
+
+        volume=newBox2.width*newBox2.depth*newBox2.height;
        
         System.out.println("Volume is " + volume);
 
