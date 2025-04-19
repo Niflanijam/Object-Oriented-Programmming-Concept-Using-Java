1270fd176b00b1a4d0a37c5aa3ad0fc17ff4f5bd
Nifla <fathimanifla1118@gmail.com>
2025-04-19 22:05:13 +0530

    user defined method

---
+++ Student.java
@@ -0,0 +1,19 @@
+class Student{
+
+public static void mynifla(){
+
+System.out.println("my new method successfully running inside the main method");
+
+}
+
+
+public static void main(String[] args){
+
+
+
+mynifla();
+
+
+}
+
+}
