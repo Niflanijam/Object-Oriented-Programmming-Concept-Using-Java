06414d032f8c222e0e4a1450e65d93b986e3fa97
Nifla <fathimanifla1118@gmail.com>
2025-04-20 23:30:08 +0530

    Inheritence

---
+++ Employee.java
@@ -0,0 +1,36 @@
+class employee
+{
+    String name="nifla";
+  
+}
+
+class company extends employee{
+
+String compant_name="ltd";
+
+
+
+
+public static void main(String[] args){
+
+company c1 =new company();
+System.out.println("new name is " + c1.name);
+System.out.println("new name is " + c1.compant_name);
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
